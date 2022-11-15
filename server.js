0b243f9b5b25302a68ce5496ae7d0e2e0be8a03e
Grant Yang <yanggra@oregonstate.edu>
2022-11-14 22:19:53 -0800

    Code blog date, title, and question

--- server.js
+++ server.js
@@ -4,3 +4,7 @@
  * name: Grant Yang
  * email: Yanggra@oregonstate.edu
  */
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(requestHandler);
+var page_index_html, page_index_js, page_404_html, page_style_css, image_benny = null;
