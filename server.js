5cd036dc6c0aa43ea9c28b0db1910b0b4d15300e
Grant Yang <yanggra@oregonstate.edu>
2022-11-14 22:21:25 -0800

    read files and store

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer(requestHandler);
 var page_index_html, page_index_js, page_404_html, page_style_css, image_benny = null;
+
+fs.readFile("./public/index.html", "utf-8", function(err, data) {
+  if (err) { throw err; }
+  page_index_html = data;
+  console.log("[file successfully read: index.html]");
+});
